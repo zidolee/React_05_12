a09965ecbcb9890b605bac3d4e1f4fad2b5cc447
ingyu yoon <a>
2019-05-11 12:00:44 +0900

    로그인 안 했을 때 로그인 페이지로 리다이렉트

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import React,{Component} from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
+import { connect } from 'react-redux'
 
 import Header from './component/header/Header'
 import HomePage from './page/home/HomePage'
 import LoginPage from './page/login/LoginPage'
 import SignupPage from './page/signup/SignupPage'
 
-class App extends Component{
 
-  render(){
+class App extends Component {
+
+  render() {
     return (
       <Router>
-        <Header/>
-        <Route path="/" exact component={HomePage} />
+        <Header />
+        <Route path="/" exact component={() => {
+            if(this.props.user){
+              return <HomePage/>
+            }else{
+              return <Redirect to="/login" />
+            }
+        }} />
         <Route path="/login" component={LoginPage} />
         <Route path="/signup" component={SignupPage} />
       </Router>
@@ -20,4 +28,10 @@ class App extends Component{
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    user: state.auth.user
+  }
+}
+
+export default connect(mapStateToProps, null)(App);
