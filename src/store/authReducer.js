15736b7220ea8f330892c62be6ace7840bab6c8c
ingyu yoon <a>
2019-05-11 11:50:32 +0900

    authReducer 추가

---
+++ src/store/authReducer.js
@@ -0,0 +1,14 @@
+
+
+
+const initialState = {
+    user: null,
+}
+
+export default function authReducer(state = initialState, action) {
+    switch (action.type) {
+
+        default:
+            return state;
+    }
+}
