17086f7b11cc61a66bc798f50729b80044e81281
ingyu yoon <a>
2019-05-11 11:36:41 +0900

    라우팅 적용

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React,{Component} from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 
-function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+import Header from './component/header/Header'
+import HomePage from './page/home/HomePage'
+import LoginPage from './page/login/LoginPage'
+import SignupPage from './page/signup/SignupPage'
+
+class App extends Component{
+
+  render(){
+    return (
+      <Router>
+        <Header/>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/signup" component={SignupPage} />
+      </Router>
+    )
+  }
 }
 
 export default App;
