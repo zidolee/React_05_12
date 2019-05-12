264d0c3a640af8c0101b2882212019fdf8c4744e
zidol <ajh3166@naver.com>
2019-05-12 13:28:03 +0900

    유저 상태 리스너

--- src/store/authReducer.js
+++ src/store/authReducer.js
@@ -1,13 +1,46 @@
 
+import firebase from 'firebase'
 
 
+
+const UPDATE_USER = 'UPDATE_USER'
+const updateUser = (user) => {
+    return  {
+        type : UPDATE_USER,
+        payload : user,
+    }
+}
+
+export const auth = () => {
+    return (dispatch) => {
+        firebase.auth().onAuthStateChanged(function(user) {
+            if (user) {
+              const newUser = {
+                  displayName : user.displayName,
+                  email : user.email,
+                  emailVerified : user.emailVerified,
+                  photoURL : user.photoURL,
+                  isAnonymous : user.isAnonymous,
+                  uid : user.uid,
+                  providerData : user.providerData
+              }
+              dispatch(updateUser(newUser));
+            } else {
+              dispatch(updateUser(null));
+            }
+          });
+    }
+}
+  
 const initialState = {
     user: null,
 }
-
 export default function authReducer(state = initialState, action) {
     switch (action.type) {
-
+        case UPDATE_USER :
+            return Object.assign({}, state, {
+                user : action.payload
+            })
         default:
             return state;
     }
