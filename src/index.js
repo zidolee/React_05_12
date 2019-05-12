7ad08d489dfec70a92777c9c2d8b84226e8f0355
zidol <ajh3166@naver.com>
2019-05-12 12:31:48 +0900

    파이어베이스 추가 및 초기화

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from "firebase";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'semantic-ui-css/semantic.min.css'
 import { configureStore } from './store/index'
 import { Provider } from 'react-redux'
+import {auth} from './store/authReducer'
+
+var config = {
+    apiKey: "AIzaSyDtMrV1aKi30GAtpRRLFupjcaGk3AQQhp0",
+    authDomain: "react-board-60426.firebaseapp.com",
+    databaseURL: "https://react-board-60426.firebaseio.com",
+    projectId: "react-board-60426",
+    storageBucket: "react-board-60426.appspot.com",
+    messagingSenderId: "543250085061",
+    appId: "1:543250085061:web:8ba82eb8024111b5"
+  };
+
+firebase.initializeApp(config);
 
 const store = configureStore();
+
+store.dispatch(auth());
 
 ReactDOM.render(
     <Provider store={store}>
